e69f62bdb6c7d33623ecf54694774cf192c968fd
Vivioli87 <vivnightingale@hotmail.com>
2021-07-05 23:03:38 +0000

    set up registration functionality

--- app.py
+++ app.py
@@ -35,8 +35,24 @@ def get_game_detail(game_id):
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username":request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
     return render_template("register.html")
-    
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
