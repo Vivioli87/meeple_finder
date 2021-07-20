fee1ec445e841722e78f3ab8b903415b3d06d915
Vivioli87 <vivnightingale@hotmail.com>
2021-07-20 21:21:51 +0000

    added 404 page and function

--- app.py
+++ app.py
@@ -544,6 +544,12 @@ def logout():
     return redirect(url_for("login"))
 
 
+# 404 handler
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
